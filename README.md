a1564896756d149074529e4213107c950e71ae5a
Sam <SamLefebvre@users.noreply.github.com>
2019-04-15 21:58:54 -0400

    Upgrade to VuePress 1.x

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# SamLefebvre.github.io
+# Welcome !
+
+This GitHub project is behind my personal website: https://samlefebvre.github.io
+
+
+Please, take a look !
+
+
+## Development
+
+This project is build on [VuePress](https://v1.vuepress.vuejs.org/) which is a Vue-powered Static Site Generator.
+
+### Steps
+
+- Clone this repository
+- `yarn install`
+- `yarn docs:dev`
+
+## Deployment
+
+
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,23 @@
 module.exports = {
-    themeConfig: {
-        nav: [
-            { text: 'Accueil', link: '/' }
-        ],
-        sidebar:[
-            '/',
-            '/page2'
-        ]
-    }    
+  themeConfig: {
+    nav: [
+        { text: 'Accueil', link: '/' },
+        { text: 'Article', link: '/article/'}
+    ],
+    sidebar: [
+        '/',
+        '/page2'
+      ],
+      updatePopup: true, // Boolean | Object, default to undefined.
+      lastUpdated: 'Last Updated', // string | boolean
+      repo: 'SamLefebvre/SamLefebvre.github.io',
+      repoLabel: 'Fork me on GitHub!',
+
+      // Optional options for generating "Edit this page" link
+      docsRepo: 'SamLefebvre/SamLefebvre.github.io',
+      docsDir: 'docs',
+      docsBranch: 'production',
+      editLinks: true,
+      editLinkText: 'Edit this page!'
+  }
 }

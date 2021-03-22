ea8c9a6d837385bb4664787c952af726f2680468
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-22 00:05:52 +0000

    Deploy SamLefebvre/SamLefebvre.github.io to github.com/SamLefebvre/SamLefebvre.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "f5c42166ef83e740d76370e18ea480de"
+    "revision": "4e2fbaa3807f9d71c004d62f84d9fc54"
   },
   {
     "url": "article/index.html",
